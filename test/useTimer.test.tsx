a660902600ce16ff53db120b33ef103a9e2ae945
Brad Garropy <bradgarropy@gmail.com>
2021-07-21 11:30:14 -0500

    initial test.

--- test/useTimer.test.tsx
+++ test/useTimer.test.tsx
@@ -1,5 +1,22 @@
+import {renderHook} from "@testing-library/react-hooks"
+
 import useTimer from "../src"
 
-test("renders", () => {
-    expect(useTimer).toBeInstanceOf(Function)
+jest.useFakeTimers()
+
+test("initializes", () => {
+    const {result} = renderHook(() => useTimer())
+
+    expect(result.current).toEqual({
+        isActive: false,
+        isInactive: true,
+        isRunning: false,
+        isPaused: false,
+        elapsedTime: 0,
+        laps: [],
+        start: expect.any(Function),
+        stop: expect.any(Function),
+        reset: expect.any(Function),
+        lap: expect.any(Function),
+    })
 })
